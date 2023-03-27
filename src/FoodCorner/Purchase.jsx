c075441b64331121670cbfdf605b3141caab0865
Santhoshkumar79 <santhoshkumarjayashankar@gmail.com>
2023-03-27 14:15:10 +0530

    purchase update

--- src/FoodCorner/Purchase.jsx
+++ src/FoodCorner/Purchase.jsx
@@ -7,12 +7,12 @@ const Purchase=()=> {
     return (
         <>
         <div className="form">
-            <form className="userform" onSubmit={ (()=> navigate("/"))}>
+            <form className="userform" onSubmit={(()=> navigate("/"))}>
             <h1>Shipping Details</h1>
             <input type="textbox" placeholder='Name' /> <br />
             <input type="textarea" placeholder='Address'  /><br />
             <input type="tel" id="phone"  name="pno" placeholder='Mobile Number' ></input><br />
-            <button className="btn" >Order</button>
+            <Button className="btn" >Order</Button>
             </form>
         </div>
         </> 
