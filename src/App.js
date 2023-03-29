eb89808fb1204dff6051d601da94a684aee91154
Santhoshkumar79 <santhoshkumarjayashankar@gmail.com>
2023-03-29 14:41:41 +0530

    task completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Meals from './FoodCorner/Meals';
-import "./FoodCorner/style.css"
-import {Routes,Route} from 'react-router-dom';
-import Receipe from './FoodCorner/Receipe';
-import  Purchase  from './FoodCorner/Purchase';
+import TodoList from './Todo/TodoList';
+
 
 const App = ()=> {
   return (
-        <>
-          <Routes>
-            <Route path="/" element={<Meals />} />
-            <Route path="/:MealId" element={<Receipe />} />
-            <Route path="/Purchase" element={<Purchase />} />
-          </Routes>
-        </>
+        <div className="App">
+        <TodoList />
+        </div>
   );
 }
 
